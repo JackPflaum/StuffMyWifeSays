4bceb16521f191e5c7ecc1c746db72ef52cbbced
Jack Pflaum <jackpflaum@gmail.com>
2023-06-28 16:42:40 +0800

    create product_details view

--- Stuff_My_Wife_Says/shop/views.py
+++ Stuff_My_Wife_Says/shop/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 from .models import Category, Product
+from .forms import AddProductToCartForm
 
 
 class HomePageView(TemplateView):
@@ -14,8 +15,24 @@ class CategoryView(TemplateView):
 
 def products(request, pk):
     category = get_object_or_404(Category, pk=pk)
-    products = Product.objects.filter(category=category).order_by('')
+    products = Product.objects.filter(category=category).order_by('price')
     return render(request, 'products.html', {'products': products})
+
+
+def product_details(request, pk):
+    """diplay product information and form for product quantity and size"""
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        form = AddProductToCartForm(request.POST)
+        if form.is_valid():
+            # do something
+            return redirect('products', pk=pk)
+    else:
+        form = AddProductToCartForm()
+        context = {'form': form, 'product': product}
+        return render(request, 'product_details.html', context)
+        
 
 
 class AboutView(TemplateView):
