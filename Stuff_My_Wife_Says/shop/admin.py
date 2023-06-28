d4f5fab29760f2918b455af73808bc66e5a4f159
Jack Pflaum <jackpflaum@gmail.com>
2023-06-28 15:05:48 +0800

    add Category and Product models to admin interface

--- Stuff_My_Wife_Says/shop/admin.py
+++ Stuff_My_Wife_Says/shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'slug']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'product_name', 'price', 'image', 'slug']
+
+    def category_name(self, obj):
+        """retrieve category_name from Category object for displaying in the Product model interface"""
+        # obj parameter represents an instance of Product model.
+        # obj.category retrieves the related 'Category' object associated with the Product instance.
+        # can therefore access category_name field.
+        return obj.category.category_name
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
