53480e0475cc3d7b4a11901149fcf0eb2e0f9601
Jack Pflaum <jackpflaum@gmail.com>
2023-07-02 16:09:56 +0800

    new shopping_cart view

--- Stuff_My_Wife_Says/shop/views.py
+++ Stuff_My_Wife_Says/shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
-from .models import Category, Product
+from .models import Category, Product, ShoppingCartSession
 from .forms import AddProductToCartForm
+from django.contrib.sessions.models import Session
 
 
 class HomePageView(TemplateView):
@@ -27,6 +28,9 @@ def product_details(request, pk):
         form = AddProductToCartForm(request.POST)
         if form.is_valid():
             # do something
+            # check is current Session has cart_uuid
+            # if yes then add item to current ShoppingCartSession
+            # if no then create cart and add cart_uuid to Session
             return redirect('products', pk=pk)
     else:
         form = AddProductToCartForm()
@@ -34,7 +38,16 @@ def product_details(request, pk):
         return render(request, 'product_details.html', context)
         
 
-
 class AboutView(TemplateView):
     """information about the company"""
     template_name = 'about_us.html'
+
+
+def shopping_cart(request):
+    # if cart_id (uuid) exists then render shopping_cart html otherwise render no_cart html
+    cart_uuid = request.Session['cart_uuid']
+    if cart_uuid is not None:
+        shopping_cart = ShoppingCartSession.objects.get(cart_uuid=cart_uuid)
+        return render(request, 'hopping_cart.html', {'shopping_cart': shopping_cart})
+    else:
+        return render(request, 'no_shopping_cart.html', {})
